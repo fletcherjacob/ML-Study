9dce3952b4da631b428a25f238503f40fe14a538
Jacob Fletcher <fletcher.c.jacob@gmail.com>
2022-03-22 10:19:58 -0500

    simpple clustering work with scatter plots

--- Classification/classification.py
+++ Classification/classification.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,9 +6,24 @@ from sklearn.datasets import make_blobs
 sbs.set()
 
 
-A = sbs.load_dataset('iris')
+X = sbs.load_dataset('iris')
 print(A)
+print(X.head())#shows default of the 1st 5 rows  Note that the first 4 columns are the features and the last is the class category label for the sample
 
-print(A.head())#shows default of the 1st 5 rows  Note that the first 4 columns are the features and the last is the class category label for the sample
-print(A.tails())#shows default of the last 5 rows
-A.iloc[60:65,:] #displays sample rows 60-64 within the data dataset
+y = X.species # pull labels from dataset
+print(y.head())
+print(y.shape)
+
+X = X.drop(columns={'species'}) #remove  labels from data set
+print(X.head())
+print(X.shape)
+
+from sklearn.svm import SVC #import Support Vector Classifier module
+
+model = SVC(gamma='auto') # Create a Support Vector Classifier
+
+model.fit(X,y) # Fit the classifier to the input training data
+
+y_pred = model.predict(X)
+
+print(y_pred)
