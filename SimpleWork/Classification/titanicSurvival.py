402986cc97649d674555443013d2f6152ff730bf
Jacob Fletcher <fletcher.c.jacob@gmail.com>
2022-03-24 15:00:03 -0500

    Added Feature Importance visualization

--- SimpleWork/Classification/titanicSurvival.py
+++ SimpleWork/Classification/titanicSurvival.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-
+from sklearn.inspection import permutation_importance
 
 #import Data
 titanicData = pd.read_csv(
@@ -43,23 +43,37 @@ titanicData = titanicData.dropna()
 
 
 #Hot Encoding for Sex Feature
-X = pd.get_dummies(titanicData, columns =['Sex'])
+X = pd.get_dummies(titanicData, columns=['Sex'])
 
 #Split Dataset into Label and Features
-X = X.drop(columns = 'Survived')
+X = X.drop(columns='Survived')
 y = titanicData['Survived']
 
+
+#Get Feature names
+feature_names = X.keys()
+
 #Split Data into Training and Testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 
 #create Classifier model
 clf = RandomForestClassifier()
 
 #train the classifier
-clf.fit(X_train,y_train)
+clf.fit(X_train, y_train)
 
 #test the classifier
 y_pred = clf.predict(X_test)
 
 # using metrics module for accuracy calculation
 print("ACCURACY OF THE MODEL: ", metrics.accuracy_score(y_test, y_pred))
+
+
+#Display feature importance
+feature_imp = pd.Series(clf.feature_importances_,
+                            index = feature_names).sort_values(ascending = False)
+
+sort = clf.feature_importances_.argsort()
+plt.barh(feature_names[sort], clf.feature_importances_[sort])
+plt.xlabel("Feature Importance")
+plt.show()
